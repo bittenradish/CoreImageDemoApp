3e9342436c461bb95834f9d3f7c0cf9cf1effc84
Rashid <40560297+Radish98@users.noreply.github.com>
2023-11-20 21:24:38 +0100

    filters. changed radius of cells

--- CoreImageDemoApp/FilterList/FilterCell.swift
+++ CoreImageDemoApp/FilterList/FilterCell.swift
@@ -13,7 +13,7 @@ class FilterCell: UICollectionViewCell {
     func commonInit() {
         backgroundColor = .white
         // Set rounded corners for the contentView
-        contentView.layer.cornerRadius = 10
+        contentView.layer.cornerRadius = 20
         contentView.layer.masksToBounds = true
         contentView.layer.borderWidth = 0
         contentView.layer.borderColor = UIColor.clear.cgColor
@@ -21,7 +21,7 @@ class FilterCell: UICollectionViewCell {
         layer.shadowOffset = CGSize(width: 2, height: 2)
         layer.shadowRadius = 4
         layer.shadowOpacity = 0.1
-        layer.cornerRadius = 10
+        layer.cornerRadius = 20
         layer.masksToBounds = false
         layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: contentView.layer.cornerRadius).cgPath
     }
