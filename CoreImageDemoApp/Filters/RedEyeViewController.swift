bc7abdfd222a81bea38c63732585ce107b747726
Rashid <40560297+Radish98@users.noreply.github.com>
2023-11-24 02:48:10 +0100

    filters. Red eye refactored. But not fixed

--- CoreImageDemoApp/Filters/RedEyeViewController.swift
+++ CoreImageDemoApp/Filters/RedEyeViewController.swift
@@ -26,24 +26,24 @@ class RedEyeViewController: UIViewController {
             
             let uiImageOrientation = inputImage?.imageOrientation ?? UIImage.Orientation.down
             
-            let orientation = CGImagePropertyOrientation(uiImageOrientation)
-            //            [CIDetectorImageOrientation: CGImagePropertyOrientation.up]
+            let orientation = CGImagePropertyOrientation(uiImageOrientation).rawValue
+            //                        [CIDetectorImageOrientation: CGImagePropertyOrientation.up]
             
-            let imageOptions: [String: Any] = [CIDetectorImageOrientation : 1]
+            let imageOptions: [String: Any] = [CIDetectorImageOrientation : orientation]
             
-        
+            
             let testFeatures = ciDetector?.features(in: ciImage, options: imageOptions)
             
             print(testFeatures ?? "empty")
             
             let options: [CIImageAutoAdjustmentOption : Any] =
             if let features = testFeatures {
-                [CIImageAutoAdjustmentOption.features: features]
+                [CIImageAutoAdjustmentOption.features: features, CIImageAutoAdjustmentOption.redEye: true]
             }else{
                 [CIImageAutoAdjustmentOption.features: ciDetector ?? CIDetector()]
             }
             
-            let filterList = ciImage.autoAdjustmentFilters(options: options)
+            let filterList = ciImage.autoAdjustmentFilters(options:options)
             filterList.forEach{ filter in
                 filter.setValue(ciImage, forKey: kCIInputImageKey)
                 if let filteredImage = filter.outputImage{
@@ -64,9 +64,8 @@ class RedEyeViewController: UIViewController {
 
 
 extension CGImagePropertyOrientation {
-    
-    init(_ orientation: UIImage.Orientation) {
-        switch orientation {
+    init(_ uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
         case .up: self = .up
         case .upMirrored: self = .upMirrored
         case .down: self = .down
@@ -75,8 +74,20 @@ extension CGImagePropertyOrientation {
         case .leftMirrored: self = .leftMirrored
         case .right: self = .right
         case .rightMirrored: self = .rightMirrored
-        @unknown default:
-            fatalError("unknown orientation passed ")
+        }
+    }
+}
+extension UIImage.Orientation {
+    init(_ cgOrientation: UIImage.Orientation) {
+        switch cgOrientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
         }
     }
 }
